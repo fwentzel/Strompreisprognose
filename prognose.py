7dc9ed21c5a6b427325524305abd8a36fc8fcd09
olafpedda <felixwentzel@gmail.com>
2020-02-20 14:29:50 +0100

    Multiple forecasts with random offsets able to plot and mean error of these

--- prognose.py
+++ prognose.py
@@ -1,4 +1,7 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
+
+
+
 from statsmodels.tsa.holtwinters import ExponentialSmoothing
 from statsmodels.tsa.seasonal import STL
 from csv_reader import get_data
@@ -6,6 +9,7 @@ import os
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+import random
 import tensorflow as tf
 import data_downloader
 
@@ -15,7 +19,7 @@ matplotlib.rcParams['figure.figsize'] = (8, 6)
 matplotlib.rcParams['axes.grid'] = False
 
 
-def single_step_predict_plot(inputs,target, title, plot=True):
+def single_step_predict_plot(inputs, target, index,plot=True,axes=None):
     predictions = []
     inputCopy = np.copy(inputs)  # copy Array to not overwrite original data
     x_in = inputCopy[:PAST_HISTORY].reshape(-1, PAST_HISTORY, inputCopy.shape[1])
@@ -30,26 +34,19 @@ def single_step_predict_plot(inputs,target, title, plot=True):
     predictions = np.array(predictions)
     error = np.around(np.sqrt(np.mean(np.square(truth - predictions))), 2)
     if plot:
-        plt.figure()
-        plt.plot(predictions, 'r', label='predictions; RMSE: {}'.format(error))
-        plt.plot(truth, 'b', label='Truth')
-        plt.legend()
-        plt.title(title)
-        plt.show()
+        axes[index].plot(predictions, 'r', label='predictions; RMSE: {}'.format(error))
+        axes[index].plot(truth, 'b', label='Truth')
+        axes[index].legend()
     return error
 
 
-def multivariate_data_single_step(_dataset, _target, start_index, end_index,
-                                  history_size, target_size):
+def multivariate_data_single_step(_dataset, _target, history_size):
     multivariate_data = []
     labels = []
-    start_index = start_index + history_size
-    if end_index is None:
-        end_index = len(_dataset) - target_size
-    for i in range(start_index + history_size, end_index):  # TODO start index nötig?
+    for i in range(history_size, len(_dataset) - 1):
         indices = range(i - history_size + 1, i + 1)
         multivariate_data.append(_dataset[indices])
-        labels.append(_target[i + target_size])
+        labels.append(_target[i + 1])
     multivariate_data = np.array(multivariate_data)
     return multivariate_data.reshape(multivariate_data.shape[0], multivariate_data.shape[1], -1), np.array(labels)
 
@@ -91,26 +88,26 @@ def initialize_network():
     # define model
     model = tf.keras.models.Sequential()
     model.add(tf.keras.layers.GRU(PAST_HISTORY,
-                                  return_sequences=True, input_shape=(x_train.shape[-2:])))
+                                  return_sequences=True, input_shape=(x.shape[-2:])))
     model.add(tf.keras.layers.GRU(int(PAST_HISTORY / 2)))
     # multi_step_model.add(tf.keras.layers.GRU(past_history))
     model.add(tf.keras.layers.Dense(FUTURE_TARGET))
     return model
 
 
-PAST_HISTORY = 12  # inputtimesteps
+PAST_HISTORY = 96  # inputtimesteps
 FUTURE_TARGET = 24  # timesteps into future
-TRAIN_LENGTH = 80  # percent
+TRAIN_LENGTH = .8  # percent
 STEP = 1
 BATCH_SIZE = 256
 BUFFER_SIZE = 500
-EPOCHS = 5
+EPOCHS = 50
 np.set_printoptions(linewidth=500, threshold=10, edgeitems=5, suppress=True)
 
 START = '2015-1-1'
 # END= date.today()
 END = '2020-02-17'
-IS_TRAINING = True  # False True
+IS_TRAINING = False  # False True
 UPDATE_DATA = False
 
 if UPDATE_DATA:
@@ -119,8 +116,7 @@ if UPDATE_DATA:
     data_downloader.update_power_price()
 
 data = get_data(start=START, end=END)
-TRAIN_SPLIT = int(len(data) / TRAIN_LENGTH)
-
+TRAIN_SPLIT = int(len(data) * TRAIN_LENGTH)
 decompose_data()
 
 # normalize the dataset using the mean and standard deviation of the training data
@@ -131,37 +127,57 @@ decompose_data()
 # Residual
 target = data["Power_Residual"]
 dataset = data[['V_N', 'SD_SO', 'F', 'Temp', 'Weekend', 'Hour', 'Holiday', 'Power_Residual']].values
-x_train, y_train = multivariate_data_single_step(dataset, target,
-                                                 0, TRAIN_SPLIT, PAST_HISTORY, 1)
-train_data = tf.data.Dataset.from_tensor_slices((x_train, y_train))
-train_data = train_data.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE).repeat()
+# x_train, y_train = multivariate_data_single_step(dataset, target,0, TRAIN_SPLIT, PAST_HISTORY, 1)
+# x_val, y_val = multivariate_data_single_step(dataset, target,TRAIN_SPLIT, len(dataset) - 50, PAST_HISTORY, 1)
 
-x_val, y_val = multivariate_data_single_step(dataset, target,
-                                             TRAIN_SPLIT, len(dataset) - 50,
-                                             PAST_HISTORY, 1)
-val_data = tf.data.Dataset.from_tensor_slices((x_val, y_val))
-val_data = val_data.batch(BATCH_SIZE).repeat()
+x, y = multivariate_data_single_step(dataset, target, PAST_HISTORY)
+
+# train_data = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+# train_data = train_data.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE).repeat()
+# val_data = tf.data.Dataset.from_tensor_slices((x_val, y_val))
+# val_data = val_data.cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE).repeat()
+
 
 multi_step_model = initialize_network()
 multi_step_model.compile(optimizer=tf.keras.optimizers.RMSprop(), loss='mae',
-                         lr=0.0003)
+                         lr=0.0001)
 
 if IS_TRAINING:
-    tf.keras.utils.Progbar.unit_name = 'schritt'
-    multi_step_history = multi_step_model.fit(train_data, epochs=EPOCHS,
-                                              steps_per_epoch=800,
-                                              validation_data=val_data,
-                                              validation_steps=300)
-    # plot_train_history(multi_step_history, 'Multi-Step Training and validation loss')
+    # multi_step_history = multi_step_model.fit(train_data, epochs=EPOCHS,
+    #                                           validation_data=val_data)
+    multi_step_history = multi_step_model.fit(x=x, y=y, epochs=EPOCHS, batch_size=BATCH_SIZE, verbose=2,
+                                              validation_split=1-TRAIN_LENGTH, shuffle=True)
+    plot_train_history(multi_step_history, 'Multi-Step Training and validation loss')
     multi_step_model.save_weights('./checkpoints/testing')
 else:
     multi_step_model.load_weights('./checkpoints/testing')
 
-# single_step_predict_plot(dataset[TRAIN_SPLIT - PAST_HISTORY - FUTURE_TARGET:TRAIN_SPLIT],
-#                          title="ON TRAINING DATA")
-single_step_predict_plot(inputs=dataset[TRAIN_SPLIT:TRAIN_SPLIT + PAST_HISTORY + FUTURE_TARGET],
-                         target=target.iloc[TRAIN_SPLIT:TRAIN_SPLIT + PAST_HISTORY + FUTURE_TARGET],
-                         title="ON TEST DATA")
+num_predicitons=100
+mean_error=0
+plot_predictions=False
+
+max=len(dataset)-PAST_HISTORY-FUTURE_TARGET-TRAIN_SPLIT
+fig, ax = plt.subplots(num_predicitons,1,sharey=True)
+
+for i in range(0,num_predicitons):
+
+    offset=random.randrange(max)
+    prediction_timeframe=slice(TRAIN_SPLIT+offset, TRAIN_SPLIT + PAST_HISTORY + FUTURE_TARGET+offset)
+
+    mean_error+=single_step_predict_plot(inputs=dataset[prediction_timeframe],
+                             target=target.iloc[prediction_timeframe],
+                             axes=ax,
+                            plot=plot_predictions,
+                             index=i)
+    print(i,mean_error/(i+1))
+    i+=1
+print("Mean error of the {0} predicitons: {1}".format(num_predicitons,mean_error/num_predicitons))
+
+if plot_predictions:plt.show()
+
+
+
+
 
 # Seasonal
 # forecast_length = 24
