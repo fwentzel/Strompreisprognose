cb1d2a9cbf8594983ff4489735e45fa7347a999e
olafpedda <felixwentzel@gmail.com>
2020-02-28 14:48:14 +0100

    Residual prediction nutzt jetzt jeweils training/test als target

--- prognose.py
+++ prognose.py
@@ -10,7 +10,7 @@ register_matplotlib_converters()
 complete_data = get_data(start='2015-1-1', end='2020-02-17', update_data=False)  # end=date.today()
 future_target = 24
 past_history = 96  # input timesteps
-start_index_from_max_length=future_target
+start_index_from_max_length=future_target+10
 use_test_data=True
 
 
@@ -26,14 +26,14 @@ else:
 # Residual
 res_pred = ResidualPrediction(train_data=train_data,test_data=test_data ,future_target=future_target, past_history=past_history, start_index_from_max_length=start_index_from_max_length)
 
-train=True
+train=False
 if train:
     res_pred.initialize_network(learning_rate=0.001)
     res_pred.train_network(savename="dense1")
 else:
     res_pred.load_model(savename="dense1")
 
-res_pred.predict(predict_test=use_test_data,num_predicitons=1, random_offset=False)
+res_pred.predict(predict_test=use_test_data, random_offset=False)
 
 # Seasonal
 seasonal_pred = SeasonalPrediction(data=data, forecast_length=future_target,
@@ -49,7 +49,7 @@ predict_from=len(data)-start_index_from_max_length
 trend_truth = data["Trend"].iloc[predict_from:predict_from + future_target]
 
 # Combine predictions
-sum_pred = res_pred.prediction_truth_error[0][0] + seasonal_pred.pred + trend_truth
+sum_pred = res_pred.pred + seasonal_pred.pred + trend_truth
 truth = data["Price"].iloc[predict_from:predict_from + future_target]
 error = np.around(np.sqrt(np.mean(np.square(truth - sum_pred))), 2)
 
@@ -60,8 +60,8 @@ seasonal_pred.plot_predictions(ax)
 ax[3].plot(trend_truth, label="Trend")
 ax[3].legend()
 ax[3].set_ylabel("TREND")
-ax[0].plot(truth, label="Price truth")
-ax[0].plot(sum_pred, label='Prediction; RMSE: {}'.format(error))
+ax[0].plot(truth.index,truth, label="Price truth")
+ax[0].plot(truth.index,sum_pred, label='Prediction; RMSE: {}'.format(error))
 ax[0].legend()
 ax[0].set_ylabel("COMBINED")
 plt.show();
--- residualPrediction.py
+++ residualPrediction.py
@@ -2,11 +2,12 @@ import random
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
+import pandas as pd
 class ResidualPrediction:
     RELEVANT_COLUMNS = ['V_N', 'SD_SO', 'F', 'Temp', 'Weekend', 'Hour',
                              'Holiday', 'Residual']
-    TRAIN_LENGTH = .8  # percent
-    BATCH_SIZE = 32
+    TRAIN_LENGTH = .6  # percent
+    BATCH_SIZE = 64
     EPOCHS = 100
 
     def __init__(self, train_data,test_data, future_target, past_history, start_index_from_max_length):
@@ -19,19 +20,16 @@ class ResidualPrediction:
         self.past_history = past_history  # inputtimesteps
 
         self.x, self.y = self.multivariate_data_single_step()
-        self.test_split = int(len(self.train_dataset) * self.TRAIN_LENGTH)
-        self.prediction_truth_error = []
 
         #TODO workarund
         self.predicted_test=False
 
     def initialize_network(self, learning_rate):
         # define model
+        #model.add(tf.keras.layers.GRU(self.past_history, return_sequences=True))
         model = tf.keras.models.Sequential()
-        model.add(tf.keras.layers.GRU(self.past_history,
-                                      return_sequences=True, input_shape=(self.x.shape[-2:])))
-        # model.add(tf.keras.layers.GRU(self.past_history))
-        model.add(tf.keras.layers.GRU(int(self.past_history / 2)))
+        model.add(tf.keras.layers.GRU(self.past_history,return_sequences=True, input_shape=(self.x.shape[-2:])))
+        model.add(tf.keras.layers.GRU(int(self.past_history / 2), return_sequences=True,dropout=0.4))
         # model.add(tf.keras.layers.Dense(self.future_target))
         model.add(tf.keras.layers.Dense(1))
         model.compile(optimizer=tf.keras.optimizers.Adam(lr=learning_rate),loss="mae")
@@ -80,35 +78,31 @@ class ResidualPrediction:
             x_in = inputCopy[j:j + self.past_history].reshape(-1, self.past_history, inputCopy.shape[1])
             x_in[-1, -1, 0] = predictions[j - 1]  # use last prediction as power Price Input for next Prediction
             predictions.append(self.model.predict(x_in)[0][-1])
-        truth = target.iloc[-self.future_target:].values
-        predictions = np.array(predictions)
-        error = np.around(np.sqrt(np.mean(np.square(truth - predictions))), 2)
+        target_rows=target.iloc[-self.future_target:]
+        self.truth = target_rows.values
+        self.pred =  pd.Series(np.array(predictions).reshape(self.future_target),index= target_rows.index)
+        self.error = np.around(np.sqrt(np.mean(np.square(self.truth - predictions))), 2)
 
-        return predictions, truth, error
 
-    def predict(self,predict_test=False, num_predicitons=2, random_offset=True):
+    def predict(self,predict_test=False, random_offset=True):
         if(predict_test ==False):
             dataset=self.train_dataset
+            target=self.train_target
         else:
             dataset = self.test_dataset
+            target = self.test_target
             self.predicted_test=True
         start= len(dataset) - self.start_index_from_max_length
         max_offset = len(dataset) - self.past_history - self.future_target - start
-        mean_error = 0
-        for i in range(0, num_predicitons):
-            if random_offset:
-                offset = random.randrange(max_offset)  # random offset for the new train_data
-            else:
-                offset = i
-            prediction_timeframe = slice(start - self.past_history - self.future_target + offset,
-                                         start + offset)
-            input=dataset[prediction_timeframe]
-            result=self.single_step_predict(inputs=input,target=self.train_target.iloc[start:start + self.future_target])
-            self.prediction_truth_error.append(result)
-            mean_error += self.prediction_truth_error[-1][2]  # error component of last prediction
-            i += 1
+        if random_offset:
+            offset = random.randrange(max_offset)  # random offset for the new train_data
+        else:
+            offset = 0
+        prediction_timeframe = slice(start - self.past_history - self.future_target + offset,
+                                     start + offset)
+        input=dataset[prediction_timeframe]
+        self.single_step_predict(inputs=input,target=target.iloc[start:start + self.future_target])
 
-        print("Mean error of the {0} predicitons: {1}".format(num_predicitons, mean_error / num_predicitons))
 
     def plot_predictions(self, ax):
         if(self.predicted_test):
@@ -118,8 +112,8 @@ class ResidualPrediction:
             start = len(self.train_dataset) - self.start_index_from_max_length
             xticks = self.train_target.index[start:start + self.future_target]
 
-        ax[1].plot(xticks, self.prediction_truth_error[0][0],
-                   label='predictions; RMSE: {}'.format(self.prediction_truth_error[0][2]))
-        ax[1].plot(xticks, self.prediction_truth_error[0][1], label='Truth')
+        ax[1].plot(xticks, self.pred,
+                   label='predictions; RMSE: {}'.format(self.error ))
+        ax[1].plot(xticks, self.truth, label='Truth')
         ax[1].legend()
         ax[1].set_ylabel("RESIDUAL")
