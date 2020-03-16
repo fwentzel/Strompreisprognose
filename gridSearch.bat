d9d1ee39cf602aab6e427de35d6debc9b4742b10
olafpedda <felixwentzel@gmail.com>
2020-03-16 14:51:23 +0100

    Bevor gridearch über extern programm

---
+++ gridSearch.bat
@@ -0,0 +1,9 @@
+for /l %%x in (1, 1, 100) do (
+   echo %%x
+   copy %%x.txt z:\whatever\etc
+)
+
+
+for past_history in range(12, 169, 12):
+for layers in range(0,6):
+for dropout in np.arange(0,6):
--- prognose.py
+++ prognose.py
@@ -2,19 +2,31 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import matplotlib.pyplot as plt
 import numpy as np
 import csv
+from argparse import ArgumentParser
 from pandas.plotting import register_matplotlib_converters
 from csv_reader import get_data
-from remainderPrediction import RemainderPrediction,LearningRateDecay
-from seasonalPrediction import SeasonalPrediction
+from neuralNetPrediction import NeuralNetPrediction,LearningRateDecay
+from statisticalPrediction import StatisticalPrediction
 import ipykernel #fix for training progressbar in Pycharm
 register_matplotlib_converters()
 
+
 future_target = 24
-past_history = 24  # input timesteps
+past_history = 60  # input timesteps
 predict_on_test_data=True
 iterations = 168 #amount of predicitons for mass predict
-step=10
-epochs=200
+step=1
+epochs=1000
+
+parser = ArgumentParser()
+parser.add_argument("-p")
+parser.add_argument("-d")
+parser.add_argument("-l")
+args = parser.parse_args()
+
+past_history=args.p
+layers=args.l
+dropout=args.d
 
 test_length=future_target+past_history+500 #300 Timesteps for testing.
 train_data,test_data=get_data(test_length=test_length, start='2015-1-1', end='2020-02-17', update_data=False)  # end=date.today()
@@ -23,69 +35,112 @@ data=test_data if predict_on_test_data else train_data
 #     data=test_data
 # else:
 #     data =train_data
+row=None
+with open("complete_results.csv") as tr:
+    reader=csv.reader(tr,delimiter=',')
+    for newrow in reader:
+        if len(newrow)>2:
+            row=newrow
+        pass
 
+# pred = RemainderPrediction(datacolumn="Price", train_data=train_data, test_data=test_data, future_target=future_target, past_history=past_history,epochs=epochs)
+# pred.load_model(savename="trainedLSTM_complete")
+# complete_error = pred.mass_predict(step=step, iterations=iterations,
+#                                                    predict_on_test_data=predict_on_test_data)
+# print("complete",complete_error)
 
-for past_history in range(168, 169, 12):
-    pred = RemainderPrediction(datacolumn="Price", train_data=train_data, test_data=test_data, future_target=future_target, past_history=past_history,epochs=epochs)
-    residual_prediction = RemainderPrediction(datacolumn="Remainder", train_data=train_data, test_data=test_data, future_target=future_target, past_history=past_history,epochs=epochs)
-    Remainder=0
+min_error=100
+min_config=None
+with open("best_config.csv") as config:
+    reader=csv.reader(config,delimiter=',')
+    for row in reader:
+        min_config=row
+        min_error=float(min_config[-1])
+
+catching_up=row is not None
+for past_history in range(12, 169, 12):
+    if catching_up:
+        if int(row[0])>past_history:
+            continue
+    pred = NeuralNetPrediction(datacolumn="Price", train_data=train_data, test_data=test_data, future_target=future_target, past_history=past_history, epochs=epochs)
+    residual_prediction = NeuralNetPrediction(datacolumn="Remainder", train_data=train_data, test_data=test_data, future_target=future_target, past_history=past_history, epochs=epochs)
     for layers in range(0,6):
-        with open('temp_results.csv', 'a', newline='') as fd:
-            writer = csv.writer(fd)
-            for dropout in np.arange(0,6):
-                dropout_decimal=dropout/10
-                train=True
-                if train:
-                    pred.initialize_network(dropout=dropout_decimal, additional_layers=layers)
-                    pred.train_network(savename="trainedLSTM_complete")
 
+        if catching_up:
+            if int(row[1]) > layers:
+                continue
+
+        for dropout in np.arange(0,6):
+            if catching_up:
+                if int(row[2])>dropout:
+                    continue
                 else:
-                    pred.load_model(savename="trainedLSTM_complete")
-                complete_error = pred.mass_predict(step=step, iterations=iterations,
-                                                       predict_on_test_data=predict_on_test_data)
-                #Remainder
+                    catching_up=False
+                    continue
+            dropout_decimal=dropout/10
 
-                train=True
-                if train:
-                    residual_prediction.initialize_network(dropout=dropout_decimal, additional_layers=layers)
-                    residual_prediction.train_network(savename="trainedLSTM_resid")
-                else:
-                    residual_prediction.load_model(savename="trainedLSTM_resid")
+            print(past_history,layers,dropout)
 
-                #res_error=residual_prediction.mass_predict(iterations=iterations,predict_on_test_data=predict_on_test_data)
+            train=True
+            if train:
+                pred.initialize_network(dropout=dropout_decimal, additional_layers=layers)
+                pred.train_network(savename="trainedLSTM_complete",save=False)
 
-                #truth=data["Price"].iloc[:iterations+future_target]
-                # plt.plot(truth.index,truth,label="Truth.")
+            else:
+                pred.load_model(savename="trainedLSTM_complete")
+            complete_error = pred.mass_predict(step=step, iterations=iterations,
+                                                   predict_on_test_data=predict_on_test_data)
+            #Remainder
 
-                # Mass predict
+            # train=True
+            # if train:
+            #     residual_prediction.initialize_network(dropout=dropout_decimal, additional_layers=layers)
+            #     residual_prediction.train_network(savename="trainedLSTM_resid")
+            # else:
+            #     residual_prediction.load_model(savename="trainedLSTM_resid")
 
-                #decomp_error=0
-                decomp_error=residual_prediction.mass_predict(step=step,iterations=iterations,predict_on_test_data=predict_on_test_data)
-                # for i in range(0,iterations,step):
-                #     # Remainder
-                #     residual_prediction.predict(predict_test=predict_on_test_data, offset=i)
-                #
-                #     # Seasonal
-                #     seasonal_pred = SeasonalPrediction(data=data, forecast_length=future_target,
-                #                                        offset=i)
-                #     seasonal_pred.predict("exp")
-                #
-                #     # Trend
-                #     predict_from=len(data)+i
-                #     trend_truth = data["Trend"].iloc[predict_from:predict_from + future_target]
-                #
-                #     # Combine predictions
-                #     sum_pred = residual_prediction.pred + seasonal_pred.pred + trend_truth
-                #
-                #     #add error
-                #     truth = data["Price"].iloc[predict_from:predict_from + future_target]
-                #     decomp_error += np.around(np.sqrt(np.mean(np.square(truth - sum_pred))), 2)
-                #
-                # decomp_error/=(iterations/step)
+            #res_error=residual_prediction.mass_predict(iterations=iterations,predict_on_test_data=predict_on_test_data)
 
+            #truth=data["Price"].iloc[:iterations+future_target]
+            # plt.plot(truth.index,truth,label="Truth.")
 
-                writer.writerow([past_history,layers,dropout,decomp_error, complete_error])
-                print(past_history,layers,dropout)
+            # Mass predict
+
+            decomp_error=0
+            # decomp_error=residual_prediction.mass_predict(step=step,iterations=iterations,predict_on_test_data=predict_on_test_data)
+            # for i in range(0,iterations,step):
+            #     # Remainder
+            #     residual_prediction.predict(predict_test=predict_on_test_data, offset=i)
+            #
+            #     # Seasonal
+            #     seasonal_pred = SeasonalPrediction(data=data, forecast_length=future_target,
+            #                                        offset=i)
+            #     seasonal_pred.predict("exp")
+            #
+            #     # Trend
+            #     predict_from=len(data)+i
+            #     trend_truth = data["Trend"].iloc[predict_from:predict_from + future_target]
+            #
+            #     # Combine predictions
+            #     sum_pred = residual_prediction.pred + seasonal_pred.pred + trend_truth
+            #
+            #     #add error
+            #     truth = data["Price"].iloc[predict_from:predict_from + future_target]
+            #     decomp_error += np.around(np.sqrt(np.mean(np.square(truth - sum_pred))), 2)
+            #
+            # decomp_error/=(iterations/step)
+            with open('complete_results.csv', 'a', newline='') as fd:
+                writer = csv.writer(fd)
+                writer.writerow([past_history,layers,dropout,complete_error])
+            if complete_error<min_error:
+                min_error=complete_error
+                min_config=[past_history,layers,dropout,min_error]
+                pred.model.save('.\checkpoints\complete_best')
+                print(min_config)
+                with open('best_config.csv', 'w', newline='') as fd:
+                    writer = csv.writer(fd)
+                    writer.writerow(min_config)
+
 
 #Plot the predictions of components and their combination with the corresponding truth
 #fig, ax = plt.subplots(4, 1, sharex=True)
